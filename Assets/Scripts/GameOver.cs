cc8a59312ed2d97bd94f38c03589e509ff31a52d
TannerH <Tanner@tanners-mbp.gateway.pace.com>
2014-04-08 23:51:48 -0600

    Final Assignment Submission

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	void Start()
+	{
+		guiTexture.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (master.fishCaught >= 25)
+		{
+			Time.timeScale = 0;
+			guiTexture.enabled = true;
+
+			if(Input.GetKey(KeyCode.R))
+			{
+				Time.timeScale = 1;
+			   	Application.LoadLevel("Scene1");
+			}
+		}
+	}
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class Shoot : MonoBehaviour
 {
-
-
-	public float speed;
 	public Rigidbody projectile;
+	public float speed;
+	public float fireRate;
+
+	private float nextFire;
+
 
 	void Update() {
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKey(KeyCode.Space) && Time.time >= nextFire)
 		{
+			nextFire = Time.time + fireRate;
 			Rigidbody clone;
 			clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
 			clone.velocity = transform.TransformDirection(Vector3.forward * speed);
